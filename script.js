dd5b52fb99c2b14cbd547a6f27396c2f77e5fca5
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-02-15 10:04:53 +0600

    more javascript code add

--- script.js
+++ script.js
@@ -15,7 +15,62 @@ function showElementById(elementId){
       element.classList.remove('hidden');
 }
 
+function getARandomAlphabet(){
+      const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+      const alphabets= alphabetString.split('');
+      
+
+      const randomNumber = Math.random()*25;
+      const index =Math.round(randomNumber);
+      const alphabet = alphabets[index];
+      return alphabet;
+}
+
+
+function setBackgroundColorById(elementId){
+      const element = document.getElementById(elementId);
+      element.classList.add('bg-orange-400')
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function continuePlaying(){
+      const alphabet = getARandomAlphabet();
+      const alphabetToUpperCase = alphabet.toUpperCase();
+
+      const showId = document.getElementById('show-alphabet');
+      showId.innerText = alphabetToUpperCase;
+      console.log(alphabet);
+
+      setBackgroundColorById(alphabet);
+}
+
 function play(){
       hiddenElementById('home-screen');
       showElementById('play-ground');
+      continuePlaying();
 }
